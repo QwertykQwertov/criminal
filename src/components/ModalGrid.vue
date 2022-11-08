d14d8b519eb6f7fca0d59029a6ade1adfa42bb14
QwertykQwertov <qwertykqwertov@mail.ru>
2022-11-08 23:12:16 +0300

    first attempt export to pdf datagrid

--- src/components/ModalGrid.vue
+++ src/components/ModalGrid.vue
@@ -9,10 +9,7 @@
     title="Судебные дела"
     @hiding="onHiding"
   >
-    <DxScrollView
-      width="100%"
-      height="100%"
-    >
+    <DxScrollView width="100%" height="100%">
       <DxDataGrid
         ref="grid"
         :data-source="store.selectQuery"
@@ -20,63 +17,41 @@
         :show-borders="true"
         :focused-row-enabled="true"
         @selection-changed="selectionChanged"
+        @exporting="onExporting"
       >
-        <DxColumn
-          data-field="data"
-          caption="Дата"
-          data-type="date"
-        />
-        <DxColumn
-          data-field="applicant"
-          caption="Истец"
-          data-type="string"
-        />
+        <DxColumn data-field="data" caption="Дата" data-type="date" />
+        <DxColumn data-field="applicant" caption="Истец" data-type="string" />
         <DxColumn
           data-field="defendant"
           caption="Ответчик"
           data-type="string"
         />
-        <DxColumn
-          data-field="law"
-          caption="Судья"
-          data-type="string"
-        />
+        <DxColumn data-field="law" caption="Судья" data-type="string" />
         <DxColumn
           data-field="category"
           caption="Категория"
           data-type="string"
         />
-        <DxColumn
-          data-field="result"
-          caption="Результат"
-          data-type="string"
-        />
+        <DxColumn data-field="result" caption="Результат" data-type="string" />
 
-        <DxMasterDetail
-          :enabled="true"
-          template="detailTemplate"
-        />
+        <DxMasterDetail :enabled="true" template="detailTemplate" />
         <template #detailTemplate="{ data: report }">
           <div class="detail-wrapper">
-            <p><b>Дата:</b> {{ report.data.data.slice(0,10) }}</p>
-            <p>
-              <b>Истец:</b> {{ report.data.applicant }}
-            </p>
-            <p>
-              <b>Ответичк:</b> {{ report.data.defendant }}
-            </p>
+            <p><b>Дата:</b> {{ report.data.data.slice(0, 10) }}</p>
+            <p><b>Истец:</b> {{ report.data.applicant }}</p>
+            <p><b>Ответичк:</b> {{ report.data.defendant }}</p>
             <p><b>Судья:</b> {{ report.data.law }}</p>
             <!-- <template style="max-width: 50px"> -->
-            <p>
-              <b>Категория:</b> {{ report.data.category }}
-            </p>
+            <p><b>Категория:</b> {{ report.data.category }}</p>
             <!-- </template> -->
-            <p>
-              <b>Результат:</b> {{ report.data.result }}
-            </p>
+            <p><b>Результат:</b> {{ report.data.result }}</p>
           </div>
         </template>
-
+        <DxExport
+          :enabled="true"
+          :formats="['pdf']"
+          :allow-export-selected-data="true"
+        />
         <DxSorting mode="multiple" />
       </DxDataGrid>
     </DxScrollView>
@@ -85,7 +60,11 @@
 
 <script>
 import DxPopup from "devextreme-vue/popup";
-import { DxScrollView } from 'devextreme-vue/scroll-view';
+import { DxScrollView } from "devextreme-vue/scroll-view";
+import { jsPDF } from "jspdf";
+import { exportDataGrid } from "devextreme/pdf_exporter";
+import { font } from "../assets/fonts/utcFontForExportPDF";
+
 import store from "../store";
 
 import {
@@ -93,7 +72,8 @@ import {
   DxColumn,
   DxSorting,
   DxSearchPanel,
-  DxMasterDetail
+  DxMasterDetail,
+  DxExport
 } from "devextreme-vue/data-grid";
 
 export default {
@@ -104,18 +84,23 @@ export default {
     DxSorting,
     DxSearchPanel,
     DxMasterDetail,
-    DxScrollView
+    DxScrollView,
+    DxExport
   },
-  data () {
+  data() {
     return {
-      store
+      store,
+      // font
     };
+  },
+  created(){
+    // this.$nextTick(()=> console.log(font))
   },
   methods: {
     // closeOutside(e) {
     //   this.store.showPopUp = false;
     // },
-    onHiding () {
+    onHiding() {
       this.store.showPopUp = false;
     },
     // contentReady(e) {
@@ -123,10 +108,24 @@ export default {
     //     e.component.selectRowsByIndexes(0);
     //   }
     // },
-    selectionChanged (e) {
+    selectionChanged(e) {
       e.component.collapseAll(-1);
       e.component.expandRow(e.currentSelectedRowKeys[0]);
     },
+    onExporting(e) {
+      const doc = new jsPDF();
+
+      doc.addFileToVFS("PTSans-Regular-normal.ttf", font);
+      doc.addFont("PTSans-Regular-normal.ttf", "PTSans-Regular", "normal");
+      doc.setFont("PTSans-Regular")
+      exportDataGrid({
+        jsPDFDocument: doc,
+        component: e.component,
+        indent: 5
+      }).then(() => {
+        doc.save("Companies.pdf");
+      });
+    }
   }
 };
 </script>
@@ -134,5 +133,8 @@ export default {
 <style scoped>
 .detail-wrapper p {
   white-space: normal;
+}
+.dx-datagrid-nowrap, .dx-datagrid-nowrap .dx-header-row>td {
+    white-space: normal !important;
 }
 </style>
